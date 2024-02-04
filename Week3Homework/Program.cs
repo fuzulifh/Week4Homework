fa4e261b4be0608f21b4ce8024fb71ffa17169fe
fuzulifh <fuzulifh@code.edu.az>
2024-02-04 14:21:21 +0400

    Homework

---
+++ Week3Homework/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Week3Homework
+{
+    internal class Program
+    {
+        static bool Main(string[] args)
+        {
+            
+            {
+                float totalSalary;
+                char familyStatus;
+                int childnumber;
+                char personHealth;
+                
+                Console.WriteLine("Enter Salary:");
+                totalSalary = char.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your Family status:(M/S/W)");
+                familyStatus = char.Parse(Console.ReadLine());
+                Console.WriteLine("Enter number of childs:");
+                childnumber=int.Parse(Console.ReadLine());
+                Console.WriteLine("Are you disable?(Y/N):");
+                personHealth = char.Parse(Console.ReadLine());
+
+                string status;
+                if (familyStatus == 'M' || familyStatus == 'S' || familyStatus == 'W') { status = "Give us disable report:"; }
+                else if { status == "You are single"; }
+                else { status = "You are happy;"; }
+
+
+                string disable;
+                if (personHealth == 'Y' || personHealth == 'M') { disable = "Give us disable report:"; }
+                else { disable = "You are good:"; }
+
+
+            }
+        }
+    }
+
+}
+    
+
